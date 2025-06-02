b2ff62fd67efcef5bf8e577b004f47f4a415563b
krsntn <kanahan0110@gmail.com>
2025-06-02 14:56:22 +0800

    update keymap

--- keyboards/crkbd/keymaps/krsntn/keymap.c
+++ keyboards/crkbd/keymaps/krsntn/keymap.c
@@ -23,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,--------------------------------------------------------------.   ,-------------------------------------------------------------.
        KC_TAB,    KC_Q,    KC_W,    KC_E,LT(3,KC_R),  KC_T,  KC_MEH,      TT(3),    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------+--------|   |-------+--------+--------+--------+--------+--------+--------|
-     CTL_T(KC_ESC),SFT_T(KC_A),OPT_T(KC_S),CMD_T(KC_D),LT(1,KC_F),KC_G,KC_HYPR,TT(5),KC_H,   KC_J,RCMD_T(KC_K),KC_L,RSFT_T(KC_SCLN),MEH_T(KC_QUOT),
+     CTL_T(KC_ESC),SFT_T(KC_A),CTL_T(KC_S),OPT_T(KC_D),CMD_T(KC_F),LT(1,KC_G),KC_HYPR,TT(5),KC_H,RCMD_T(KC_J),OPT_T(KC_K),RCTL_T(KC_L),RSFT_T(KC_SCLN),MEH_T(KC_QUOT),
   //|--------+--------+--------+--------+--------+--------+--------'   `-------+--------+--------+--------+--------+--------+--------|
       KC_LSFT,    KC_Z,LT(5, KC_X), KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_HYPR,
   //|--------+--------+--------+--------+--------+--------+--------.  ,--------+--------+--------+--------+--------+--------+--------|
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,--------------------------------------------------------------.   ,-------------------------------------------------------------.
       _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,    _______, XXXXXXX, KC_PGUP, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|   |-------+--------+--------+--------+--------+--------+--------|
-      _______, _______, _______, _______, _______, XXXXXXX, _______,    _______, KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, XXXXXXX, XXXXXXX,
+      _______, _______, _______, _______, _______, _______, _______,    _______, KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------'   `-------+--------+--------+--------+--------+--------+--------|
       _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_HOME, KC_PGDN, XXXXXXX,  KC_END, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------.  ,--------+--------+--------+--------+--------+--------+--------|
@@ -47,23 +47,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,--------------------------------------------------------------.   ,-------------------------------------------------------------.
       _______, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC, _______,    _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------+--------|   |-------+--------+--------+--------+--------+--------+--------|
-      _______, _______, _______, _______, KC_EXLM,   KC_AT, _______,    _______, KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS,  KC_GRV,
+      _______, _______, _______, _______, _______, _______, _______,    _______, KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS,  KC_GRV,
   //|--------+--------+--------+--------+--------+--------+--------'   `-------+--------+--------+--------+--------+--------+--------|
       _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_TILD,
   //|--------+--------+--------+--------+--------+--------+--------.  ,--------+--------+--------+--------+--------+--------+--------|
-                                          _______, _______, _______,    _______, _______, _______
+                                          _______, _______, _______,    KC_EXLM,   KC_AT, _______
                                       //`--------------------------'  `--------------------------'
   ),
 
     [3] = LAYOUT_split_3x6_3_ex2(
   //,--------------------------------------------------------------.   ,-------------------------------------------------------------.
-      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,    _______, XXXXXXX,   KC_7,   KC_8,   KC_9, XXXXXXX, _______,
+      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,    _______, XXXXXXX,    KC_7,    KC_8,    KC_9, XXXXXXX, _______,
   //|--------+--------+--------+--------+--------+--------+--------|   |-------+--------+--------+--------+--------+--------+--------|
-      _______, _______, _______, _______, _______, XXXXXXX, _______,    _______, XXXXXXX,   KC_4,   KC_5,   KC_6, XXXXXXX, XXXXXXX,
+      _______, _______, _______, _______, _______, _______, _______,    _______, XXXXXXX,    KC_4,    KC_5,    KC_6, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------'   `-------+--------+--------+--------+--------+--------+--------|
-      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX,   KC_1,   KC_2,   KC_3, XXXXXXX, XXXXXXX,
+      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX,    KC_1,    KC_2,    KC_3, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          _______, _______, _______,      KC_0, _______, _______
+                                          _______, _______, _______,       KC_0, _______, _______
                                       //`--------------------------'  `--------------------------'
   ),
 
@@ -71,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,--------------------------------------------------------------.   ,-------------------------------------------------------------.
       KC_CAPS,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5, _______,    _______,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_DEL,
   //|--------+--------+--------+--------+--------+--------+--------|   |-------+--------+--------+--------+--------+--------+--------|
-      _______, _______, _______, _______, _______, XXXXXXX, _______,    _______, XXXXXXX, XXXXXXX, XXXXXXX,  KC_F11,  KC_F12,  KC_MEH,
+      _______, _______, _______, _______, _______, _______, _______,    _______, XXXXXXX, XXXXXXX, XXXXXXX,  KC_F11,  KC_F12,  KC_MEH,
   //|--------+--------+--------+--------+--------+--------+--------'   `-------+--------+--------+--------+--------+--------+--------|
       _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_BRID, KC_BRIU, KC_VOLD, KC_VOLU, KC_MUTE, KC_HYPR,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
@@ -83,7 +83,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,--------------------------------------------------------------.   ,-------------------------------------------------------------.
       _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,    _______, XXXXXXX, KC_WH_U, KC_MS_U, XXXXXXX, XXXXXXX, _______,
   //|--------+--------+--------+--------+--------+--------+--------|   |-------+--------+--------+--------+--------+--------+--------|
-      _______, _______, _______, _______, _______, XXXXXXX, _______,    _______, KC_WH_L, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_R, XXXXXXX,
+      _______, _______, _______, _______, _______, _______, _______,    _______, KC_WH_L, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_R, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------'   `-------+--------+--------+--------+--------+--------+--------|
       _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, KC_WH_D, XXXXXXX, KC_BTN2, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
@@ -94,12 +94,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case SFT_T(KC_A):    // left home row
-        case OPT_T(KC_S):    // left home row
-        case GUI_T(KC_D):    // left home row
-        case MEH_T(KC_QUOT): // right home row
-        case RCMD_T(KC_K):    // right home row
-        case ROPT_T(KC_L):    // right home row
+        case SFT_T(KC_A): // left home row
+        case CTL_T(KC_S): // left home row
+        case OPT_T(KC_D): // left home row
+        case CMD_T(KC_F): // left home row
+        case RCMD_T(KC_J): // right home row
+        case OPT_T(KC_K): // right home row
+        case RCTL_T(KC_L): // right home row
         case RSFT_T(KC_SCLN): // right home row
             return TAPPING_TERM + 60;
         default:
@@ -119,8 +120,9 @@ uint16_t get_quick_tap_term(uint16_t keycode, keyrecord_t *record) {
 bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case LT(1, KC_F):
-        case LT(3, KC_R):
         case LT(1, KC_ENT):
+        case LT(3, KC_R):
+        case LT(5, KC_X):
             // Immediately select the hold action when another key is tapped.
             return true;
         default:
